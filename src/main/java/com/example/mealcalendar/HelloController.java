0cb0ed2e3c089e961f089cc2618c25e06377f671
LucaSabatiniTorVergata <luca.sabatini@students.uniroma2.eu>
2025-01-28 12:40:17 +0100

    Sonar cloud aggiunto

--- src/main/java/com/example/mealcalendar/HelloController.java
+++ src/main/java/com/example/mealcalendar/HelloController.java
@@ -1,14 +1,37 @@
 package com.example.mealcalendar;
 
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import java.io.IOException;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
 
 public class HelloController{
 
+    @FXML
+    private Button loginbutton;
 
+    @FXML
+    private TextField passInChiaro;
 
+    @FXML
+    private PasswordField passWordBox;
 
+    @FXML
+    void LoadLoginPage(ActionEvent event) throws IOException {
 
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("register-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280,720);
+
+        Stage stage = (Stage) fxmlLoader.getRoot().getScene().getWindow();
+        stage.setScene(scene);
+
+    }
 
 }
