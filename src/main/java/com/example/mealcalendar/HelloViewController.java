c74415421bb4ac77ac917f44f7ba5a0bbcb5a6c4
SMITH <smithvalenzuela324@gmail,com>
2025-01-29 15:06:44 +0100

    aggiungo prime azione dei bottoni

---
+++ src/main/java/com/example/mealcalendar/HelloViewController.java
@@ -0,0 +1,35 @@
+package com.example.mealcalendar;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+public class HelloViewController {
+
+    @FXML
+    private void guestmenuview(ActionEvent event) throws IOException {
+        Parent NuovaSchermata = FXMLLoader.load(getClass().getResource("guestmenu-view.fxml"));
+        Scene NuovaScena=new Scene(NuovaSchermata);
+        Stage Finestra=(Stage)((Node)event.getSource()).getScene().getWindow();
+        Finestra.setScene(NuovaScena);
+        Finestra.show();
+    }
+    @FXML
+    private void loginview(ActionEvent event) throws IOException {
+        Parent NuovaSchermata = FXMLLoader.load(getClass().getResource("login-view.fxml"));
+        Scene NuovaScena=new Scene(NuovaSchermata);
+        Stage Finestra=(Stage)((Node)event.getSource()).getScene().getWindow();
+        Finestra.setScene(NuovaScena);
+        Finestra.show();
+    }
+}
+
+
+
+
+
--- src/main/java/com/example/mealcalendar/RegisterLoginPageController.java
+++ src/main/java/com/example/mealcalendar/RegisterLoginPageController.java
@@ -1,18 +1,23 @@
 package com.example.mealcalendar;
 
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 
-    import javafx.event.ActionEvent;
-    import javafx.fxml.FXML;
-    import javafx.scene.control.Button;
-
-    public class RegisterLoginPageController {
+public class RegisterLoginPageController {
 
         @FXML
-        private Button goBackButton;
-
-        @FXML
-        void goBack(ActionEvent event) {
-
+        private void indietroview(ActionEvent event) throws IOException {
+            Parent NuovaSchermata = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+            Scene NuovaScena=new Scene(NuovaSchermata);
+            Stage Finestra=(Stage)((Node)event.getSource()).getScene().getWindow();
+            Finestra.setScene(NuovaScena);
+            Finestra.show();
 
         }
 
