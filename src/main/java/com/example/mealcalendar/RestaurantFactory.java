a5768c33027a6fc713a1769ece8b0f1d9630c0ec
SMITH <smithvalenzuela324@gmail,com>
2025-02-03 12:11:08 +0100

    soluzione pat

--- src/main/java/com/example/mealcalendar/RestaurantFactory.java
+++ src/main/java/com/example/mealcalendar/RestaurantFactory.java
@@ -2,6 +2,9 @@ package com.example.mealcalendar;
 
 public class RestaurantFactory {
 
+    private RestaurantFactory () {
+    }
+
     public static RestaurantEntity CreaRistorante(String Nome, String Indirizzo, double Lat, double Lng) {
         return new RestaurantEntity(Nome, Indirizzo, Lat, Lng);
     }
