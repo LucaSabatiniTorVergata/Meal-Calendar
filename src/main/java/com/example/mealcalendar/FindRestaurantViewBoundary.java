1f92f6193e821b9a100760de15874b593a4f377c
LucaSabatiniTorVergata <luca.sabatini@students.uniroma2.eu>
2025-02-03 15:02:50 +0100

    code smell sistemati

--- src/main/java/com/example/mealcalendar/FindRestaurantViewBoundary.java
+++ src/main/java/com/example/mealcalendar/FindRestaurantViewBoundary.java
@@ -2,6 +2,8 @@ package com.example.mealcalendar;
 
 import javafx.scene.input.MouseEvent;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -21,8 +23,9 @@ import java.util.regex.Pattern;
 import javafx.scene.control.TextFormatter;
 import java.util.function.UnaryOperator;
 
-
 public class FindRestaurantViewBoundary {
+
+    private static final Logger LOGGER = Logger.getLogger(FindRestaurantViewBoundary.class.getName());
 
     @FXML
     private SplitMenuButton tipoDieta;
@@ -46,7 +49,7 @@ public class FindRestaurantViewBoundary {
     private MenuItem cena;
 
     @FXML
-    private ListView<ReturnRestaurantsBean> ristorantiListView;// ListView per mostrare i ristoranti
+    private ListView<ReturnRestaurantsBean> ristorantiListView;
 
     private String tipoDietaSelezionato;
     private String pastoSelezionato;
@@ -54,92 +57,30 @@ public class FindRestaurantViewBoundary {
 
     private List<ReturnRestaurantsBean> listaRistoranti;
 
-
-    @FXML
-    private void loadHomePageGuest(ActionEvent event) throws IOException {
-        Parent nuovaSchermata = FXMLLoader.load(getClass().getResource("guestmenu-view.fxml"));
-        Scene nuovaScena = new Scene(nuovaSchermata);
-        Stage finestra = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        finestra.setScene(nuovaScena);
-        finestra.show();
-
-    }
-
-    @FXML
-    private void registerGuest(ActionEvent event) throws IOException {
-        Parent nuovaSchermata = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
-        Scene nuovaScena = new Scene(nuovaSchermata);
-        Stage finestra = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        finestra.setScene(nuovaScena);
-        finestra.show();
-
-    }
-
-    @FXML
-    private void loadHomePageUser(ActionEvent event) throws IOException {
-        Parent nuovaSchermata = FXMLLoader.load(getClass().getResource("usermenu-view.fxml"));
-        Scene nuovaScena = new Scene(nuovaSchermata);
-        Stage finestra = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        finestra.setScene(nuovaScena);
-        finestra.show();
-    }
-
-    @FXML
-    private void recipeuser(ActionEvent event) throws IOException {
-        Parent nuovaSchermata = FXMLLoader.load(getClass().getResource("recipe-view.fxml"));
-        Scene nuovaScena = new Scene(nuovaSchermata);
-        Stage finestra = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        finestra.setScene(nuovaScena);
-        finestra.show();
-    }
-
-    @FXML
-    private void fridgeuser(ActionEvent event) throws IOException {
-        Parent nuovaSchermata = FXMLLoader.load(getClass().getResource("fridge-view.fxml"));
-        Scene nuovaScena = new Scene(nuovaSchermata);
-        Stage finestra = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        finestra.setScene(nuovaScena);
-        finestra.show();
-    }
-
-    @FXML
-    private void calendaruser(ActionEvent event) throws IOException {
-        Parent nuovaSchermata = FXMLLoader.load(getClass().getResource("mealcalendar-view.fxml"));
-        Scene nuovaScena = new Scene(nuovaSchermata);
-        Stage finestra = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        finestra.setScene(nuovaScena);
-        finestra.show();
-    }
-
     @FXML
     private void confermafiltri(ActionEvent event) throws IOException {
-        // Solo dopo aver premuto "conferma" copiamo i dati
-        if(tipoDieta.getText().equalsIgnoreCase("Onnivoro")){
+        if (tipoDieta.getText().equalsIgnoreCase("Onnivoro")) {
             tipoDietaSelezionato = "";
-
-        }
-        else {
+        } else {
             tipoDietaSelezionato = tipoDieta.getText();
         }
 
         pastoSelezionato = pasto.getText();
-        distanzaInserita= Double.parseDouble(distanza.getText());
-        System.out.println("Filtri confermati:");
-        System.out.println("Dieta: " + tipoDietaSelezionato);
-        System.out.println("Pasto: " + pastoSelezionato);
-        System.out.println("Distanza: " + distanzaInserita + " km");
+        distanzaInserita = Double.parseDouble(distanza.getText());
+
+        LOGGER.log(Level.INFO, "Filtri confermati:");
+        LOGGER.log(Level.INFO, "Dieta: {0}", tipoDietaSelezionato);
+        LOGGER.log(Level.INFO, "Pasto: {0}", pastoSelezionato);
+        LOGGER.log(Level.INFO, "Distanza: {0} km", distanzaInserita);
+
         FiltersRestaurantBean filtro = new FiltersRestaurantBean(tipoDietaSelezionato, pastoSelezionato, distanzaInserita);
-
-        ChooseRestaurantController Controller = new ChooseRestaurantController(filtro);
-        List<ReturnRestaurantsBean> RistorantiBeans = Controller.trovaRistorante();
-
-        mostraRistoranti(RistorantiBeans);;
-
+        ChooseRestaurantController controller = new ChooseRestaurantController(filtro);
+        List<ReturnRestaurantsBean> ristorantiBeans = controller.trovaRistorante();
+        mostraRistoranti(ristorantiBeans);
     }
 
     @FXML
     public void initialize() throws IOException {
-        // Associare le azioni ai MenuItem (cambiano il testo del bottone)
         vegan.setOnAction(e -> tipoDieta.setText("Vegano"));
         vegetariana.setOnAction(e -> tipoDieta.setText("Vegetariano"));
         omnivora.setOnAction(e -> tipoDieta.setText("Onnivoro"));
@@ -147,49 +88,37 @@ public class FindRestaurantViewBoundary {
         colazione.setOnAction(e -> pasto.setText("Colazione"));
         pranzo.setOnAction(e -> pasto.setText("Pranzo"));
         cena.setOnAction(e -> pasto.setText("Cena"));
-        Pattern validEditingState = Pattern.compile("\\d*"); // Solo cifre
 
-        UnaryOperator<TextFormatter.Change> filter = change -> {
-            if (validEditingState.matcher(change.getControlNewText()).matches()) {
-                return change; // Accetta la modifica
-            } else {
-                return null; // Rifiuta la modifica se contiene caratteri non numerici
-            }
-        };
+        Pattern validEditingState = Pattern.compile("\\d*");
+        UnaryOperator<TextFormatter.Change> filter = change -> validEditingState.matcher(change.getControlNewText()).matches() ? change : null;
 
         TextFormatter<String> textformatter = new TextFormatter<>(filter);
         distanza.setTextFormatter(textformatter);
     }
-    //Metodo per mostrare i ristoranti nella ListView
-    public void mostraRistoranti(List<ReturnRestaurantsBean> ListaRistoranti) {
+
+    public void mostraRistoranti(List<ReturnRestaurantsBean> LlstaRistoranti) {
         ristorantiListView.getItems().clear();
-        this.listaRistoranti=ListaRistoranti;
-        ristorantiListView.getItems().addAll(ListaRistoranti);
+        this.listaRistoranti = listaRistoranti;
+        ristorantiListView.getItems().addAll(listaRistoranti);
     }
-    //Metodo per aprire Google Maps nel browser con le coordinate
-        private void apriGoogleMaps(double lat, double lng) {
-            String url = "https://www.google.com/maps/search/?api=1&query=" + lat + "," + lng;
-            try {
-                Desktop.getDesktop().browse(new URI(url)); // Apriamo il browser
-            } catch (IOException | URISyntaxException e) {
-                e.printStackTrace();
-            }
+
+    private void apriGoogleMaps(double lat, double lng) {
+        String url = "https://www.google.com/maps/search/?api=1&query=" + lat + "," + lng;
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (IOException | URISyntaxException e) {
+            LOGGER.log(Level.SEVERE, "Errore nell'apertura di Google Maps", e);
         }
+    }
 
-
-    //Metodo chiamato quando l'utente clicca su un ristorante
     @FXML
     private void handleclick(MouseEvent event) {
-        if (event.getClickCount() == 2) { // Doppio click per aprire Google Maps
+        if (event.getClickCount() == 2) {
             int selectedIndex = ristorantiListView.getSelectionModel().getSelectedIndex();
             if (selectedIndex >= 0) {
-                ReturnRestaurantsBean Ristorante = listaRistoranti.get(selectedIndex);
-                apriGoogleMaps(Ristorante.getLatitudine(), Ristorante.getLongitudine());
+                ReturnRestaurantsBean ristorante = listaRistoranti.get(selectedIndex);
+                apriGoogleMaps(ristorante.getLatitudine(), ristorante.getLongitudine());
             }
         }
     }
-
-
-
 }
-
