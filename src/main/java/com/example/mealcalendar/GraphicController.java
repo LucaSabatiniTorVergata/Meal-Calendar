c91c9800cb412f74e3c2cce22454f8271b863822
SMITH <smithvalenzuela324@gmail,com>
2025-02-17 13:58:59 +0100

    nuovo SessionManagerSLT

--- src/main/java/com/example/mealcalendar/GraphicController.java
+++ src/main/java/com/example/mealcalendar/GraphicController.java
@@ -16,22 +16,12 @@ public class GraphicController {
         try {
             FXMLLoader loader = new FXMLLoader(GraphicController.class.getResource(fxmlview));
             Parent root = loader.load();
-
-            // Recupera il controller della nuova scena
-            Object controller = loader.getController();
-
-            // Se il controller ha i metodi setUseDB e setUseDemo, imposta i valori
-            if (controller instanceof PassaParametri) {
-                ((PassaParametri) controller).setUseDB(useDB);
-                ((PassaParametri) controller).setUseDemo(useDemo);
-            }
-
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
-            System.out.println("sborrar");
         } catch (IOException e) {
             e.printStackTrace();
+
         }
     }
 }
