c158dde223d815a55674a3dccc79b7f5f18624ba
LucaSabatiniTorVergata <luca.sabatini@students.uniroma2.eu>
2025-02-20 15:38:18 +0100

    test effettuati

--- src/test/java/com/example/mealcalendar/FindRestaurantControllerTest.java
+++ src/test/java/com/example/mealcalendar/FindRestaurantControllerTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FindRestaurantTestController {
+class FindRestaurantControllerTest {
 
     private ChooseRestaurantController controller;
     private RestaurantListSLTPat listaRistoranti;
