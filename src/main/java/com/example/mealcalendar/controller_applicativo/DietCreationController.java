2b6f40e3e2f1793b9755c4b0044993b8430fee87
SMITH <smithvalenzuela324@gmail,com>
2025-08-22 10:42:32 +0200

    aggiunto exeception

--- src/main/java/com/example/mealcalendar/controller_applicativo/DietCreationController.java
+++ src/main/java/com/example/mealcalendar/controller_applicativo/DietCreationController.java
@@ -3,16 +3,30 @@ package com.example.mealcalendar.controller_applicativo;
 
 import com.example.mealcalendar.dao.DietDAO;
 import com.example.mealcalendar.bean.DietBean;
+import com.example.mealcalendar.handlexceptions.DietNotFoundException;
+import com.example.mealcalendar.handlexceptions.DietPersistenceException;
 import com.example.mealcalendar.model.DayEntity;
 import com.example.mealcalendar.model.DietEntity;
 import com.example.mealcalendar.model.MealEntity;
 
+import java.util.logging.Logger;
+
 public class DietCreationController {
+
+    private static final Logger logger = Logger.getLogger(DietCreationController.class.getName());
 
     public void saveDiet(DietBean diet) {
 
-        DietDAO.getInstance().saveDiet(converter(diet));
-        DietDAO.getInstance().getAllDiets();
+        try {
+            DietDAO.getInstance().saveDiet(converter(diet));
+            DietDAO.getInstance().getAllDiets();
+            logger.info("Dieta salvata con successo!");
+        } catch (DietPersistenceException e) {
+            logger.info("Errore di persistenza: " + e.getMessage());
+        } catch (DietNotFoundException e) {
+            logger.info("Errore: " + e.getMessage());
+        }
+
     }
 
     public DietEntity converter(DietBean diet) {
