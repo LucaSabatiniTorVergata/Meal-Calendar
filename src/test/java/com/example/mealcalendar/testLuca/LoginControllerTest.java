61206d31496fec8e90b3f5fb271cbd2cbdad7a46
LucaSabatiniTorVergata <luca.sabatini@students.uniroma2.eu>
2025-08-25 19:20:18 +0200

    puzzette levate

--- src/test/java/com/example/mealcalendar/testLuca/LoginControllerTest.java
+++ src/test/java/com/example/mealcalendar/testLuca/LoginControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.mealcalendar.testLuca;
 
+import com.example.mealcalendar.exception.LoginException;
 import com.example.mealcalendar.register_login.LoginController;
 import com.example.mealcalendar.register_login.UserLoginBean;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +18,7 @@ class LoginControllerTest {
     }
 
     @Test
-    void testValloginUtente() {
+    void testValloginUtente() throws LoginException {
         UserLoginBean user = new UserLoginBean("testUser", "password", "user");
         boolean result = controller.vallogin(user);
         // Poiché non c'è alcun utente registrato, ci aspettiamo false
@@ -25,7 +26,7 @@ class LoginControllerTest {
     }
 
     @Test
-    void testValloginRistorante() {
+    void testValloginRistorante() throws LoginException {
         UserLoginBean ristorante = new UserLoginBean("DemoRistorante", "password", "restaurant");
         boolean result = controller.vallogin(ristorante);
         // Deve essere true se la login crea il demo ristorante
